7fe149dc5f829380496a0f0b9e9a815cc39ad716
Conor Cunningham <conor.cunningham@gmail.com>
2023-09-12 20:49:54 +0200

    log INFO messages

--- multi_host_ping_tool/ping_and_log.py
+++ multi_host_ping_tool/ping_and_log.py
@@ -13,20 +13,20 @@ if not os.path.isfile(log_file):
 
 
 # Create a custom logger
-logging.basicConfig()
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Create handlers
 stream_handler = logging.StreamHandler()
 file_handler = logging.FileHandler(log_file)
-stream_handler.setLevel(logging.DEBUG)
-file_handler.setLevel(logging.DEBUG)
-# logging.basicConfig(level=logging.INFO, format='%(message)s')
+stream_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
 
 # Create formatters and add it to handlers
 stream_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 # file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_format = logging.Formatter('%(asctime)s - %(message)s')
+
 stream_handler.setFormatter(stream_format)
 file_handler.setFormatter(file_format)
 
@@ -35,13 +35,13 @@ logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
 
-async def ping_and_log(host):
+async def ping_and_log(host) -> ping:
     ping_result = ping(host, count=1, privileged=False)
     await parse_result(ping_result)
     return ping_result
 
 
-async def parse_result(result):
+async def parse_result(result) -> None:
     if result.is_alive:
         logger.info(f"{result.address} is alive")
     else:
--- ping_hosts.py
+++ ping_hosts.py
@@ -11,16 +11,29 @@ if not os.path.isfile(hosts_file):
     Path(hosts_file).touch()
 
 
-async def queue_tasks(queue):
-    # tasks = []
+async def queue_tasks(queue) -> None:
+    """
+    This coroutine will read the hosts file and put each host in the queue.
+    In a Python queue example, this would be the producer
+    All we are doing here is adding a host (an IP address) to the queue
+    :param queue:
+    :return: None
+    """
     for host in read_hosts():
         await queue.put(host)
-        # tasks.append(ping_and_log(host))
     await queue.put(None)
-    # await asyncio.gather(*tasks)
 
 
-def read_hosts():
+def read_hosts() -> list:
+    """
+    This function will read the hosts file and return a list of hosts
+    It expects the hosts file to contain a single IP address per line
+    e.g.,
+    192.168.1.100
+    192.168.2.100
+    192.168.3.100
+    :return: Lists of IP addresses
+    """
     with open(hosts_file, "r") as infile:
         hosts = infile.readlines()
         hosts = [host.strip() for host in hosts if host is not None]
@@ -28,8 +41,13 @@ def read_hosts():
 
 
 # coroutine to consume work
-async def consumer(queue):
-    print('Consumer: Running')
+async def consumer(queue) -> None:
+    """
+    Pull hosts from the queue and send them to ping_and_log
+    :param queue:
+    :return: None
+    """
+    print('Ping tasks: Running')
     # consume work
     while True:
         # get a unit of work
@@ -37,18 +55,17 @@ async def consumer(queue):
         # check for stop signal
         if host is None:
             break
-        # report
+        # pass hosts to the ping test method
         await ping_and_log(host)
-    # all done
-    print('Consumer: Done')
+    print('Ping Tasks: Done. Check log file for results')
 
 
-async def main():
+async def main() -> None:
     # create the shared queue
     queue = asyncio.Queue(maxsize=30)
     # run the producer and consumers
     await asyncio.gather(queue_tasks(queue), consumer(queue))
 
 
-# if __name__ == "__main__":
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
